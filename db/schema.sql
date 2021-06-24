fb54962a945ee783cac127bff494324a4e65086f
rrogers422 <Reagan422440@gmail.com>
2021-06-24 14:51:21 -0400

    refactored schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,30 @@
 DROP DATABASE IF EXISTS employee_db;
+CREATE database employee_db;
 
-/* Create database */
-CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE department_table (
-    id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+CREATE TABLE dept_table (
+    dept_id INT AUTO_INCREMENT NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (dept_id)
 );
 
 CREATE TABLE role_table (
-    id INT NOT NULL AUTO_INCREMENT,
+    role_id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL ,
-    department_id INT, 
-    PRIMARY KEY(id),
-    FOREIGN KEY(department_id) REFERENCES department_table(id)
+    salary DECIMAL NOT NULL,
+    dept_id INT NOT NULL,
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (dept_id) REFERENCES dept_table(dept_id)
 );
 
 CREATE TABLE employee_table (
-    id INT NOT NULL AUTO_INCREMENT,
+    employee_id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY(role_id) REFERENCES role_table(id)
-);
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (role_id) REFERENCES role_table (role_id),
+    FOREIGN KEY (manager_id) REFERENCES employee_table (employee_id)
+)
